3e44b994162c4d84c5e652293f0bddc69a24a571
amit murmu <your.akmurmu6580@google.com>
2024-10-05 21:24:26 +0530

    1. backend server setup 2. frontend setup done

---
+++ back-end/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const connectDB = require("./config/db");
+
+const server = express();
+const port = process.env.PORT;
+const mongoUri = process.env.MONGO_URI;
+// Middlewares
+server.use(express.json());
+server.use(cors());
+
+// Home Route
+server.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to 1mg-clone server." });
+});
+
+// Startig the server
+server.listen(port, async () => {
+  try {
+    await connectDB(mongoUri);
+    console.log(
+      `Database is connected and Server is listening on port: ${port}`
+    );
+  } catch (error) {
+    console.log(`Something went wrong while starting the server!`, error);
+  }
+});
---
+++ front-end/src/App.jsx
@@ -0,0 +1,10 @@
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+// import "./App.css";
+import { Box } from "@chakra-ui/react";
+
+function App() {
+  return <Box>This is 1mg clone</Box>;
+}
+
+export default App;
